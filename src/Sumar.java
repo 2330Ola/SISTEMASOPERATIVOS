b205310b1224bebbb1656328e31b924180da063b
2330Ola <jolao@miumg.edu.gt>
2024-08-02 23:15:55 -0600

    Version 2

---
+++ src/Sumar.java
@@ -0,0 +1,9 @@
+// Sumar.java
+public class Sumar {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 3;
+        int resultado = num1 + num2;
+        System.out.println("La suma es: " + resultado);
+    }
+}
